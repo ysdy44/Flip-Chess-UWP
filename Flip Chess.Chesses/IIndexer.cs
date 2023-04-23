1136538b39c2fce848bf82543df15aeda33cdecc
ysdy44 <ysdy44@outlook.com>
2023-04-24 01:38:38 +0800

    Chesses Add IIndexer

---
+++ Flip Chess.Chesses/IIndexer.cs
@@ -0,0 +1,12 @@
+﻿namespace Flip_Chess.Chesses
+{
+    public interface IIndexer
+    {
+        int Step { get; }
+        bool IsRed { get; }
+        bool IsBlack { get; }
+
+        int Index { get; set; }
+        ChessType[,,] Collection { get; }
+    }
+}
