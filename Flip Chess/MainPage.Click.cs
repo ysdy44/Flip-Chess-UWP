f740214b41aed61b3b746496fb60336272e8483b
ysdy44 <ysdy44@outlook.com>
2023-04-24 02:10:04 +0800

    Flip Chess Add MainPage.Click

---
+++ Flip Chess/MainPage.Click.cs
@@ -0,0 +1,32 @@
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.AutoAIs;
+using Flip_Chess.Chesses.Extensions;
+using Flip_Chess.Models;
+using System;
+using System.Windows.Input;
+
+namespace Flip_Chess
+{
+    partial class MainPage
+    {
+
+        //@Delegate
+        public event EventHandler CanExecuteChanged;
+
+        //@Command
+        public bool CanExecute(object parameter) => parameter != default;
+        public void Execute(object parameter)
+        {
+            if (parameter is OptionType item)
+            {
+                this.Click(item);
+            }
+        }
+
+        public ICommand Command => this;
+        public void Click(OptionType type)
+        {
+        }
+
+    }
+}
--- Flip Chess/MainPage.xaml.cs
+++ Flip Chess/MainPage.xaml.cs
@@ -1,8 +1,17 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.AutoAIs;
+using Flip_Chess.Chesses.Extensions;
+using Flip_Chess.Elements;
+using Flip_Chess.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Flip_Chess
 {
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, ICommand
     {
         public MainPage()
         {
