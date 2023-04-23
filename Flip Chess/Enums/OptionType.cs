a10855803a8aac49ee979d241168cc6df316fd02
ysdy44 <ysdy44@outlook.com>
2023-04-24 02:01:51 +0800

    Flip Chess Add Enums

---
+++ Flip Chess/Enums/OptionType.cs
@@ -0,0 +1,32 @@
+﻿namespace Flip_Chess
+{
+    public enum OptionType
+    {
+        Ai,
+        Play,
+
+        Lose,
+        Win,
+        Pause,
+
+        Continue,
+        Restart,
+        Next,
+
+        FullScreen,
+        UnFullScreen,
+
+        About,
+        Tutorial,
+
+        Step1,
+        Step2,
+        Step3,
+        Step4,
+
+        UIFlipCompleted,
+        UICaptureCompleted,
+        UICemeteryCompleted,
+        UIClipCompleted,
+    }
+}
