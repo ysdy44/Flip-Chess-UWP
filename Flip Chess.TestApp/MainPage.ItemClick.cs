68bbbe61f5759716e7e7d711cee2fd02401791f5
ysdy44 <ysdy44@outlook.com>
2023-04-24 01:57:43 +0800

    TestApp Add MainPage.ItemClick

---
+++ Flip Chess.TestApp/MainPage.ItemClick.cs
@@ -0,0 +1,54 @@
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.Extensions;
+
+namespace Flip_Chess.TestApp
+{
+    partial class MainPage
+    {
+        public void ItemClick(History history)
+        {
+            this.Timer.Stop();
+            this.Timer.Start();
+
+            this.Historian.Add(history);
+
+            switch (history.Action)
+            {
+                case HistoryAction.Noway:
+                    {
+                        this.Randoms.Random();
+                        this.Chesses.Clear();
+                        this.Collection.Clear();
+
+                        this.Historian.Clear();
+                    }
+                    break;
+                case HistoryAction.Flip:
+                    {
+                        // Info
+                        int index = this.Collection.IndexOf(history.Y, history.X);
+                        ChessType random = this.Randoms[index].Type;
+                        this.Collection[0, history.Y, history.X] = random;
+
+                        // UI
+                        this.Chesses.Copy(this.Collection);
+                    }
+                    break;
+                case HistoryAction.Capture:
+                    {
+                        // Info
+                        ChessType fromType = this.Collection[0, history.Y1, history.X1];
+
+                        this.Collection[0, history.Y1, history.X1] = ChessType.Deaded;
+                        this.Collection[0, history.Y2, history.X2] = fromType;
+
+                        // UI
+                        this.Chesses.Copy(this.Collection);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Flip Chess.TestApp/MainPage.xaml.cs
+++ Flip Chess.TestApp/MainPage.xaml.cs
@@ -1,12 +1,69 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.AutoAIs;
+using Flip_Chess.Chesses.Extensions;
+using Flip_Chess.TestApp.Models;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Flip_Chess.TestApp
 {
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, IIndexer
     {
+        readonly DispatcherTimer Timer = new DispatcherTimer
+        {
+            Interval = System.TimeSpan.FromSeconds(1)
+        };
+
+        // History
+        public int Step => this.Historian.Count;
+        public bool IsRed => this.Step % 2 == 0; // Index
+        public bool IsBlack => this.Step % 2 != 0; // Index
+        public IList<History> Historian { get; } = new List<History>();
+
+        // Collection
+        public int Index { get; set; } // Index
+        public ChessType[,,] Collection { get; } = new ChessType[1024, 8, 4]; // Sertings // Index
+        public Chess[] Chesses { get; } = new Chess[8 * 4]
+        {
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+        };
+        public Chess[] Randoms { get; } = new Chess[8 * 4] // Sertings
+        {
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+            new Chess(), new Chess(), new Chess(), new Chess(),
+        };
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Randoms.Random();
+
+            this.Timer.Tick += (s, e) =>
+            {
+                if (this.IsRed)
+                {
+                    this.ItemClick(new RedAutoAICollection(this).FindAutoAI(this));
+                }
+                else if (this.IsBlack)
+                {
+                    this.ItemClick(new BlackAutoAICollection(this).FindAutoAI(this));
+                }
+            };
+            this.Timer.Start();
         }
     }
 }
