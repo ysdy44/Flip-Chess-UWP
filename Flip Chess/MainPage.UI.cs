e0413c72ee8942fd5a853205f5310d50317ca0a2
ysdy44 <ysdy44@outlook.com>
2023-04-24 02:13:20 +0800

    Flip Chess Add MainPage.UI

---
+++ Flip Chess/MainPage.UI.cs
@@ -0,0 +1,24 @@
+﻿using Flip_Chess.Chesses;
+using Flip_Chess.Chesses.Extensions;
+using Flip_Chess.Models;
+using System;
+using System.Numerics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Flip_Chess
+{
+    partial class MainPage
+    {
+        #region DependencyProperty
+
+        public GameState State
+        {
+            get => (GameState)base.GetValue(StateProperty);
+            set => base.SetValue(StateProperty, value);
+        }
+        public static readonly DependencyProperty StateProperty = DependencyProperty.Register(nameof(State), typeof(GameState), typeof(MainPage), new PropertyMetadata(GameState.None));
+
+        #endregion
+    }
+}
