7e0a899fec437f161f658656e43d1beb52be7aa3
ysdy44 <ysdy44@outlook.com>
2023-04-24 23:57:21 +0800

    Add HistoryDistance and HistoryRelation

--- Flip Chess.Chesses/Enums/HistoryAction.cs
+++ Flip Chess.Chesses/Enums/HistoryAction.cs
@@ -2,9 +2,9 @@
 {
     public enum HistoryAction
     {
-        Noway,
-
-        Flip,
-        Capture,
+        Noway = 0,
+        Flip = 1,
+        Capture = 2,
+        Select = 3,
     }
 }
