6101bbade7eaeb3bb6089164cd408ed8411ef53e
ysdy44 <ysdy44@outlook.com>
2023-04-29 01:29:20 +0800

    Fix bugs: CollectionExtensions

--- Flip Chess.Chesses/Extensions/CollectionExtensions.Black.cs
+++ Flip Chess.Chesses/Extensions/CollectionExtensions.Black.cs
@@ -1,60 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace Flip_Chess.Chesses.Extensions
+﻿namespace Flip_Chess.Chesses.Extensions
 {
     partial class CollectionExtensions
     {
-        public static IEnumerable<History> GetBlackHistory(this ChessType[,,] array, int zIndex)
-        {
-            int h = array.Height();
-            int w = array.Width();
-
-            for (int y = 0; y < h; y++)
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    ChessType item = array[zIndex, y, x];
-                    if (item.IsBlack() is false) continue;
-
-                    if (x > 0)
-                    {
-                        ChessType left = array[zIndex, y, x - 1];
-                        if (item.BlackRelateTo(left) is HistoryRelation.WeakEnemy)
-                        {
-                            yield return new History(y, x, y, x - 1);
-                        }
-                    }
-
-                    if (y > 0)
-                    {
-                        ChessType top = array[zIndex, y - 1, x];
-                        if (item.BlackRelateTo(top) is HistoryRelation.WeakEnemy)
-                        {
-                            yield return new History(y, x, y - 1, x);
-                        }
-                    }
-
-                    if (x + 1 < w)
-                    {
-                        ChessType right = array[zIndex, y, x + 1];
-                        if (item.BlackRelateTo(right) is HistoryRelation.WeakEnemy)
-                        {
-                            yield return new History(y, x, y, x + 1);
-                        }
-
-                        if (y + 1 < h)
-                        {
-                            ChessType bottom = array[zIndex, y + 1, x];
-                            if (item.BlackRelateTo(bottom) is HistoryRelation.WeakEnemy)
-                            {
-                                yield return new History(y, x, y + 1, x);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
         public static NeighborType GetBlackNeighbor(this ChessType[,,] array, int zIndex, int y, int x)
         {
             int h = array.Height();
--- Flip Chess.Chesses/Extensions/CollectionExtensions.Red.cs
+++ Flip Chess.Chesses/Extensions/CollectionExtensions.Red.cs
@@ -1,60 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace Flip_Chess.Chesses.Extensions
+﻿namespace Flip_Chess.Chesses.Extensions
 {
     partial class CollectionExtensions
     {
-        public static IEnumerable<History> GetRedHistory(this ChessType[,,] array, int zIndex)
-        {
-            int h = array.Height();
-            int w = array.Width();
-
-            for (int y = 0; y < h; y++)
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    ChessType item = array[zIndex, y, x];
-                    if (item.IsRed() is false) continue;
-
-                    if (x > 0)
-                    {
-                        ChessType left = array[zIndex, y, x - 1];
-                        if (item.RedRelateTo(left) is HistoryRelation.WeakEnemy)
-                        {
-                            yield return new History(y, x, y, x - 1);
-                        }
-                    }
-
-                    if (y > 0)
-                    {
-                        ChessType top = array[zIndex, y - 1, x];
-                        if (item.RedRelateTo(top) is HistoryRelation.WeakEnemy)
-                        {
-                            yield return new History(y, x, y - 1, x);
-                        }
-                    }
-
-                    if (x + 1 < w)
-                    {
-                        ChessType right = array[zIndex, y, x + 1];
-                        if (item.RedRelateTo(right) is HistoryRelation.WeakEnemy)
-                        {
-                            yield return new History(y, x, y, x + 1);
-                        }
-
-                        if (y + 1 < h)
-                        {
-                            ChessType bottom = array[zIndex, y + 1, x];
-                            if (item.RedRelateTo(bottom) is HistoryRelation.WeakEnemy)
-                            {
-                                yield return new History(y, x, y + 1, x);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
         public static NeighborType GetRedNeighbor(this ChessType[,,] array, int zIndex, int y, int x)
         {
             int h = array.Height();
