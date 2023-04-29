9d114525a20d41d120404180d98d1e83dee93b90
ysdy44 <ysdy44@outlook.com>
2023-04-29 15:21:01 +0800

    Elements Add CommandListBox and CommandListView

---
+++ Flip Chess.Elements/CommandListBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Flip_Chess.Elements
+{
+    public sealed class CommandListBox : ListBox
+    {
+        public object CommandParameter { get; set; }
+        public ICommand Command { get; set; }
+        public CommandListBox()
+        {
+            base.SelectionChanged += (s, e) =>
+            {
+                this.Command?.Execute(this.CommandParameter); // Command
+            };
+        }
+    }
+}
---
+++ Flip Chess.Elements/CommandListView.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Flip_Chess.Elements
+{
+    public sealed class CommandListView : ListView
+    {
+        public ICommand Command { get; set; }
+        public CommandListView()
+        {
+            base.ItemClick += (s, e) =>
+            {
+                this.Command?.Execute(e.ClickedItem); // Command
+            };
+        }
+    }
+}
